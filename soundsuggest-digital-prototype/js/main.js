9787fce4f616a565fbc46a1929453a6e37c4c2bf
JorisSchelfaut <joris.schelfaut@hotmail.com>
2013-04-13 12:21:26 +0200

    update json and user info

--- soundsuggest-digital-prototype/js/main.js
+++ soundsuggest-digital-prototype/js/main.js
@@ -51,5 +51,37 @@ function itemInfo(itemname, isrecommendation, user) {
 }
 
 function userInfo(userName, isActiveUser, activeuser) {
-    
+    var html = '';
+    var userinfo;
+    if (! isActiveUser) {
+        lastfm.user.getInfo({
+            user      : userName
+        }, {
+            success : function(response1) {
+                userinfo = response1;
+                lastfm.tasteometer.compare({
+                    value1  : activeuser,
+                    value2  : userName,
+                    type1   : 'user',
+                    type2   : 'user'
+                }, {
+                    success : function(response2) {
+                        var score = Number(response2.comparison.result.score) * 100;
+                        html += '<p>The similarity score between you and ' + userName
+                                + ' equals ' + score.toFixed(2)
+                                + '%.</p>';
+                        html += '<p>Click <a href="' + userinfo.user.url
+                                + '" target="_blank" title="' + userinfo.user.name
+                                + '\'s profile">here</a> to visit the full profile.</p>';
+                        jQuery('#user-info-description')
+                            .append(html);
+                    },
+                    error : function(error) {}
+                });
+            },
+            error : function(error) {}
+        });
+    } else {
+        // Active user's profile.
+    }
 }
